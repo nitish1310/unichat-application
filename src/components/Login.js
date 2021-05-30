259353ac28d12e2d7e286d315f4314a0eab08b06
Nitish Soman <nitish.soman13@gmail.com>
2021-05-30 12:52:19 -0400

    Add AuthContext

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+import "firebase/app";
+
+import { auth } from "../firebase";
+import firebase from "firebase/app";
 
 const Login = () => {
   return (
@@ -7,14 +11,24 @@ const Login = () => {
       <div id="login-card">
         <h2>Welcome to Unichat!</h2>
 
-        <div className="login-button google">
+        <div
+          className="login-button google"
+          onClick={() =>
+            auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+          }
+        >
           <GoogleOutlined /> Sign In with Google
         </div>
 
         <br />
         <br />
 
-        <div className="login-button facebook">
+        <div
+          className="login-button facebook"
+          onClick={() =>
+            auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())
+          }
+        >
           <FacebookOutlined /> Sign In with Facebook
         </div>
       </div>
